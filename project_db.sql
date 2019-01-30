cc8cbac4de502cd7be6137d1c2386e242453b7e6
Titam <itsmystyles9889@gmail.com>
2019-01-30 14:53:41 +0700

    data base update

--- project_db.sql
+++ project_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 15, 2019 at 03:51 AM
+-- Generation Time: Jan 30, 2019 at 08:51 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -47,7 +47,9 @@ INSERT INTO `data_anggota_perpus` (`id_anggota`, `nama_anggota`, `alamat_anggota
 (15, 'titam', 'SUBANG', 'Laki-Laki', '08126523236', '2019-01-11 10:26:37'),
 (16, 'M Bintang F', 'Subang', 'Laki-Laki', '08923642312', '2019-01-11 10:26:58'),
 (17, 'Restu Adi', 'Subang', 'Laki-Laki', '08563524623', '2019-01-11 10:27:22'),
-(18, 'luli', 'subang', 'Wanita', '081324941403', '2019-01-14 09:37:52');
+(18, 'luli', 'subang', 'Wanita', '081324941403', '2019-01-14 09:37:52'),
+(19, 'Bintang', 'Subang', 'Laki-Laki', '0928237823', '2019-01-18 14:41:30'),
+(20, 'aku', 'subang', 'Wanita', '0721766243', '2019-01-27 04:28:18');
 
 -- --------------------------------------------------------
 
@@ -69,16 +71,15 @@ CREATE TABLE `data_peminjaman` (
 --
 
 INSERT INTO `data_peminjaman` (`id_pjn`, `id_anggota_peminjaman`, `kode_buku_pjn`, `jumlah_hari_pjn`, `tgl_pengembalian`, `tanggal_entri_pjn`) VALUES
-(58, 12, 20, 12, 'January 26, 2019, 7:19 am', 'January 14, 2019, 7:19 am'),
-(59, 18, 23, 2, 'January 16, 2019, 8:09 am', 'January 14, 2019, 8:09 am'),
-(60, 12, 21, 4, 'January 18, 2019, 10:43 am', 'January 14, 2019, 10:43 am'),
-(61, 12, 21, 4, 'January 18, 2019, 10:44 am', 'January 14, 2019, 10:44 am'),
 (62, 12, 22, 81, 'April 5, 2019, 10:45 am', 'January 14, 2019, 10:45 am'),
 (63, 15, 22, 2000, 'July 6, 2024, 10:46 am', 'January 14, 2019, 10:46 am'),
 (64, 13, 20, 1, 'January 15, 2019, 10:46 am', 'January 14, 2019, 10:46 am'),
 (65, 12, 21, 11, 'January 25, 2019, 10:46 am', 'January 14, 2019, 10:46 am'),
 (66, 13, 23, 1, 'January 15, 2019, 10:46 am', 'January 14, 2019, 10:46 am'),
-(67, 12, 21, 1, 'January 15, 2019, 10:47 am', 'January 14, 2019, 10:47 am');
+(67, 12, 21, 1, 'January 15, 2019, 10:47 am', 'January 14, 2019, 10:47 am'),
+(68, 12, 21, 5, 'January 20, 2019, 12:01 pm', 'January 15, 2019, 12:01 pm'),
+(69, 15, 20, 10, 'January 25, 2019, 12:03 pm', 'January 15, 2019, 12:03 pm'),
+(70, 13, 22, 1, 'January 27, 2019, 10:16 pm', 'January 26, 2019, 10:16 pm');
 
 -- --------------------------------------------------------
 
@@ -106,7 +107,11 @@ INSERT INTO `list_buku` (`kode_buku`, `gambar_buku`, `judul_buku`, `penerbit`, `
 (20, 'IMG-20180823-WA0022.jpg', 'Kisah 36 Remaja', 'Gramedia', 2019, '108', 'mengisah kan 36 teman yaitu satu kelas dengan 1 tokoh utama', '2019-01-09 11:08:48', 'Titam'),
 (21, '2018-08-14 (2).png', 'baruuu', 'Gramedia', 2091, '100', 'rame', '2019-01-10 09:34:45', 'jkds'),
 (22, '2018-08-14 (2).png', 'inc.', 'tata inc.', 2002, '23', 'Kisah inc.', '2019-01-11 14:04:31', 'Irfan'),
-(23, 'Adobe_Post_20180819_161931.jpg', 'TAP TO Edit', 'Titam', 2002, '203', 'Kisah Tap To edit', '2019-01-14 13:16:12', 'Titak');
+(23, 'Adobe_Post_20180819_161931.jpg', 'TAP TO Editkdjfds', 'Titam', 2002, '203', 'Kisah Tap To edit', '2019-01-14 13:16:12', 'Titak'),
+(24, 'cutlery.png', 'kisah sendok', 'Titam', 2002, '22', 'Kisah Sendok dan Garpu', '2019-01-17 17:22:09', 'Septian'),
+(25, 'book-slide-1.jpg', 'Kisah Banyak Buku', 'pt. ayam sejahtera', 2002, '230', 'Kisah Buku - Buku yang berada di perpusatkaan bersama buku buku lainnya yang saling bersahabat', '2019-01-18 11:20:24', 'Titam'),
+(26, 'foto3.jpg', 'Kita', 'pt. ayam sejahtera', 2009, '134', 'Kita adalah kami', '2019-01-18 14:10:16', 'Septian'),
+(27, 'play-button.png', 'play Button', 'pt ayam', 2002, '201', 'Tentang Play Button', '2019-01-29 11:50:49', 'Titam');
 
 -- --------------------------------------------------------
 
@@ -122,6 +127,17 @@ CREATE TABLE `riwayat` (
   `jumlah_hari_pjn_ryt` int(255) NOT NULL,
   `tgl_pengembalian_ryt` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `riwayat`
+--
+
+INSERT INTO `riwayat` (`id_pjn_ryt`, `id_anggota_ryt`, `kode_buku_ryt`, `tgl_pjn_ryt`, `jumlah_hari_pjn_ryt`, `tgl_pengembalian_ryt`) VALUES
+(58, 12, 20, 'January 14, 2019, 7:19 am', 12, 'January 15, 2019, 10:29 am'),
+(59, 18, 23, 'January 14, 2019, 8:09 am', 3, 'January 17, 2019, 11:27 am'),
+(60, 12, 21, 'January 14, 2019, 10:43 am', 4, 'January 26, 2019, 10:29 pm'),
+(61, 12, 21, 'January 14, 2019, 10:44 am', 4, 'January 26, 2019, 10:29 pm'),
+(71, 15, 20, 'January 26, 2019,', 12, 'January 26, 2019, 10:28 pm');
 
 -- --------------------------------------------------------
 
@@ -187,19 +203,19 @@ ALTER TABLE `user_login`
 -- AUTO_INCREMENT for table `data_anggota_perpus`
 --
 ALTER TABLE `data_anggota_perpus`
-  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `data_peminjaman`
 --
 ALTER TABLE `data_peminjaman`
-  MODIFY `id_pjn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id_pjn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `list_buku`
 --
 ALTER TABLE `list_buku`
-  MODIFY `kode_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `kode_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
