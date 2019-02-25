e0f6ceb9d3fa54805e7cbaab9b7bf65559b2b994
Titam <itsmystyles9889@gmail.com>
2019-02-25 09:12:13 +0700

    Update anggota-db.php

--- page/dataanggota/anggota-db.php
+++ page/dataanggota/anggota-db.php
@@ -3,12 +3,12 @@
 include "../../koneksi.php";
 
 
+
 $angoota = $_GET['id'] ?? '';
 
 $wh = "";
 $wh .= !empty($angoota) ?"id_anggota = '{$angoota}' and " : "";
 $wh = !empty($wh) ? "where ".substr($wh, 0, -4) : "";
-
 
 $view = $mysqli->query("SELECT * FROM data_anggota_perpus {$wh}");
 $row = $view->fetch_all(MYSQLI_ASSOC);
